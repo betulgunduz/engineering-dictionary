4a77303922be38d4bc57921d4e41db2c0e7fa9d9
betulgunduz <betulgunduz6@gmail.com>
2018-11-16 15:04:15 +0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class TranslateInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            myWord: "",
+            translatedWord: ""
+        }
+    }
+
+    dict = {
+        engineer: 'mühendis',
+        mühendis: 'engineer'
+
+    }
+
+    clickTranslate() {
+        let val = this.dict[this.state.myWord];
+        this.setState({translatedWord: val})
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={(e) => {
+                    this.setState({myWord: e.target.value.toLowerCase()})
+                }}/>
+                <h1>{this.state.myWord}</h1>
+                <button onClick={() => {
+                    this.clickTranslate()
+                }}>Translate!!
+                </button>
+                <h1>{this.state.translatedWord}</h1>
+            </div>
+        );
+    }
+}
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="container">
+                    <TranslateInput/>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
